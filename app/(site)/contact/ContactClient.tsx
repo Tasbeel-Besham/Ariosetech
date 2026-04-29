7efc41c7fe4c0d771a4a19d67e9102209613c441
Tasbeel-Besham <T.besham@gmail.com>
2026-04-30 03:24:13 +0500

    fix: import useEffect in ContactClient

--- app/(site)/contact/ContactClient.tsx
+++ app/(site)/contact/ContactClient.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { ArrowRight, Mail, Phone, MapPin, Clock, MessageCircle } from '@/components/ui/Icons'
 import ClutchWidget from '@/components/ui/ClutchWidget'
