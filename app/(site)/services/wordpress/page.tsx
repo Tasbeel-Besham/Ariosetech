0bdfa00c4ebbae14c5a44078f525be64c0d04e2f
Tasbeel-Besham <T.besham@gmail.com>
2026-05-01 18:10:37 +0500

    chore: trigger deployment heartbeat

--- app/(site)/services/wordpress/page.tsx
+++ app/(site)/services/wordpress/page.tsx
@@ -1,3 +1,4 @@
+/* Deployment Heartbeat: 2026-05-01 18:10:30 */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Link from 'next/link'
 import { getCollection } from '@/lib/db/mongodb'
